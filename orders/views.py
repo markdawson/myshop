7e69597d2a0c982795402a0ef7ada8a945a91f13
Mark Dawson <mark@markdawson.xyz>
2016-08-15 13:14:37 -0400

    adding rabbitmq and celery

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart 
+from .tasks import order_created
 
 def order_create(request):
 	cart = Cart(request)
@@ -11,11 +12,13 @@ def order_create(request):
 			order = form.save()
 			for item in cart:
 				OrderItem.objects.create(order=order,
-										product=item['product'],
-										price=item['price'],
-										quantity=item['quantity'])
+							product=item['product'],
+							price=item['price'],
+							quantity=item['quantity'])
 			# clear the cart
 			cart.clear()
+			# launch asynchronous task
+			order_created.delay(order.id)
 			return render(request, 'orders/order/created.html',
 						{'order': order})
 
